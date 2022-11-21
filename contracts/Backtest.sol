341f8c5d9cda81a2353afe6bd688fc9c36116b00
fmrmf <m@fmrmf.xyz>
2022-11-21 18:23:45 -0500

    fix natspec title for backtest

--- contracts/Backtest.sol
+++ contracts/Backtest.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+/// @title Backtest Strategy Wrapper
+/// @notice Abstract Backtest wrapper to execute on-chain strategies through for back (and forward) testing
 abstract contract Backtest {
     constructor() {}
     
