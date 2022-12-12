8fa58f10e0604833eb335b93f5a14cef7e76f2f0
fmrmf <m@fmrmf.xyz>
2022-12-12 00:57:46 -0500

    fix lint

--- tests/test_backtest.py
+++ tests/test_backtest.py
@@ -23,24 +23,23 @@ def test_multicall(backtest, WETH9, acc):
     targets = [WETH9.address, WETH9.address]
     datas = [
         WETH9.deposit.as_transaction().data,
-        WETH9.withdraw.as_transaction(1000000000000000000).data
+        WETH9.withdraw.as_transaction(1000000000000000000).data,
     ]
     values = [1000000000000000000, 0]  # 1 wad
-    total_value = sum(values)
+    tv = sum(values)
     prior_acc_bal = acc.balance
 
     # multicall deposit => withdraw through backtester
-    receipt = backtest.multicall(
-        targets, datas, values, value=total_value, sender=acc)
+    receipt = backtest.multicall(targets, datas, values, value=tv, sender=acc)
 
     # check no balance of WETH in backtester
     assert WETH9.balanceOf(backtest) == 0
 
     # check backtester credited with 1 ETH
-    assert backtest.balance == total_value
+    assert backtest.balance == tv
 
     # check the eth came from acc
-    assert acc.balance == prior_acc_bal - total_value - receipt.total_fees_paid
+    assert acc.balance == prior_acc_bal - tv - receipt.total_fees_paid
 
 
 def test_value(backtest, acc):
