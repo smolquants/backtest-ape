b491e1366975c817c1d0b6cca653b453616e48f6
fmrmf <m@fmrmf.xyz>
2022-12-11 22:13:26 -0500

    add mock backtest

---
+++ contracts/mocks/MockBacktest.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {Backtest} from "../Backtest.sol";
+
+/// @title Mock Backtest
+/// @notice Strategy Backtest measuring ETH value in contract
+contract MockBacktest is Backtest {
+    function value() public view virtual override returns (uint256) {
+        return address(this).balance;
+    }
+}
--- tests/helpers/mocks/MockBacktest.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
-
-/// @title Mock Aave Strategy Backtest
-/// @notice Strategy Backtest measuring value as Aave collateral less Aave debt in ETH terms
-contract MockAaveBacktest is Backtest {
-    
-}
