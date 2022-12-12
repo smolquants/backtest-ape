6abfa38a1a965240ed2b5984fc090a73a99fad5f
fmrmf <m@fmrmf.xyz>
2022-12-11 23:44:21 -0500

    test: value

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ape import project
+from ape import Contract, project
 
 
 @pytest.fixture(scope="module")
@@ -11,3 +11,8 @@ def acc(accounts):
 @pytest.fixture(scope="module")
 def backtest(acc):
     yield project.MockBacktest.deploy(sender=acc)
+
+
+@pytest.fixture(scope="module")
+def WETH9():
+    yield Contract("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
--- tests/test_backtest.py
+++ tests/test_backtest.py
@@ -1,6 +1,14 @@
-def test_execute(backtest):
+def test_execute(backtest, acc):
     pass
 
 
-def test_multicall(backtest):
+def test_multicall(backtest, acc):
     pass
+
+
+def test_value(backtest, acc):
+    assert backtest.value() == 0
+
+    # send 1 eth in to backtester
+    acc.transfer(backtest.address, 1000000000000000000)  # 1 wad
+    assert backtest.value() == 1000000000000000000
