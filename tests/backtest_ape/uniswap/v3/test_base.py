e99876b746c62fac81228c1155b3f934d783714f
fmrmf <m@fmrmf.xyz>
2022-12-21 12:15:43 -0500

    Revert "use mock token A and B"
    This reverts commit 850bb1c7c2cac6a1b1fb4fa3d2218cab56c30b63.

--- tests/backtest_ape/uniswap/v3/test_base.py
+++ tests/backtest_ape/uniswap/v3/test_base.py
@@ -5,12 +5,7 @@ from backtest_ape.uniswap.v3.base import BaseUniswapV3Runner
 
 @pytest.fixture
 def runner():
-    return BaseUniswapV3Runner(
-        ref_addrs={
-            "pool": "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8",
-            "weth": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-        }
-    )
+    return BaseUniswapV3Runner()
 
 
 def test_setup(runner, acc):
@@ -20,19 +15,19 @@ def test_setup(runner, acc):
     mocks = runner._mocks
     assert set(mocks.keys()) == set(
         [
-            "tokenA",
-            "tokenB",
+            "weth",
+            "token",
             "factory",
             "manager",
             "pool",
         ]
     )
-    assert mocks["tokenA"].symbol() == "MOKA"
-    assert mocks["tokenB"].symbol() == "MOKB"
+    assert mocks["weth"].symbol() == "WETH"
+    assert mocks["token"].symbol() == "MOK"
     assert mocks["factory"].feeAmountTickSpacing(3000) == 60
     assert mocks["manager"].factory() == mocks["factory"].address
     assert mocks["pool"].fee() == 3000
     assert (
-        mocks["factory"].getPool(mocks["tokenA"].address, mocks["tokenB"].address, 3000)
+        mocks["factory"].getPool(mocks["weth"].address, mocks["token"].address, 3000)
         == mocks["pool"].address
     )
