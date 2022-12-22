ea1a27cffb313666e663d2989e0efe0499836e7a
fmrmf <m@fmrmf.xyz>
2022-12-22 07:54:46 -0500

    account for decimals in tricrypto

--- contracts/curve/v2/CurveTricryptoLPBacktest.sol
+++ contracts/curve/v2/CurveTricryptoLPBacktest.sol
@@ -4,16 +4,24 @@ pragma solidity 0.8.17;
 import {Backtest} from "../../Backtest.sol";
 import {ICurveV2Pool} from "./interfaces/ICurveV2Pool.sol";
 import {ICurveV2Token} from "./interfaces/ICurveV2Token.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 /// @title Curve Tricrypto Liquidity Provider Backtester
 /// @notice Backtests an LP position in pool reporting value of LP tokens in USDT terms
 contract CurveTricryptoLPBacktest is Backtest {
     ICurveV2Pool public immutable pool;
     ICurveV2Token public immutable lp;
+    uint256 public immutable numCoins;
+    uint8[] public decimals;
 
-    constructor(address _pool) {
+    constructor(address _pool, uint256 _numCoins) {
         pool = ICurveV2Pool(_pool);
         lp = ICurveV2Token(pool.token());
+        numCoins = _numCoins;
+
+        for (uint256 i = 0; i < _numCoins; ++i) {
+            decimals.push(IERC20Metadata(pool.coins(i)).decimals());
+        }
     }
 
     /// @notice Reports the USDT value of the LP token
@@ -36,7 +44,7 @@ contract CurveTricryptoLPBacktest is Backtest {
 
             // convert values to USDT denom using oracle price
             uint256 price = pool.price_oracle(i - 1);
-            value_ += ((price * balance) / 1e30); // 1e18 * 1e12 for mulDiv then convert to 6 decimals
+            value_ += ((price * balance * 10 ** (decimals[0])) / (1e18 * 10 ** (decimals[i]))); // mulDiv and decimal conversion
         }
     }
 }
