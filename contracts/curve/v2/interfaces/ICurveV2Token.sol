ae36c9844d0507337a20b821b7dced95f8dd0270
fmrmf <m@fmrmf.xyz>
2022-12-25 18:42:24 -0500

    lint solidity

--- contracts/curve/v2/interfaces/ICurveV2Token.sol
+++ contracts/curve/v2/interfaces/ICurveV2Token.sol
@@ -7,7 +7,7 @@ interface ICurveV2Token {
     function totalSupply() external view returns (uint256);
 
     function mint(address _to, uint256 _value) external returns (bool);
-    
+
     function mint_relative(address _to, uint256 frac) external returns (uint256);
 
     function burnFrom(address _to, uint256 _value) external returns (bool);
