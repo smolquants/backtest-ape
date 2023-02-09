1572fdaf995508cd4c7e24ca9dac4b9c3fe2770e
fmrmf <m@fmrmf.xyz>
2023-02-09 18:20:33 -0500

    feat: finish contract for gearbox v2 backtest

--- contracts/gearbox/v2/GearboxV2CABacktest.sol
+++ contracts/gearbox/v2/GearboxV2CABacktest.sol
@@ -2,11 +2,24 @@
 pragma solidity ^0.8.10;
 
 import {Backtest} from "../../Backtest.sol";
+import {ICreditFacade} from "@gearbox/core-v2/contracts/interfaces/ICreditFacade.sol";
+import {ICreditManagerV2} from "@gearbox/core-v2/contracts/interfaces/ICreditManagerV2.sol";
 
 /// @title Gearbox V2 Credit Account Backtester
 /// @notice Backtests a credit account strategy reporting value in underlying token terms
 contract GearboxV2CABacktest is Backtest {
-    constructor() {}
+    address public immutable facade;
+    address public immutable manager;
 
-    function value() public view virtual override returns (uint256 value_) {}
+    constructor(address _facade, address _manager) {
+        facade = _facade;
+        manager = _manager;
+    }
+
+    /// @notice Reports the current value of the credit account
+    /// @return value_ The current value of the credit account
+    function value() public view virtual override returns (uint256 value_) {
+        address account = ICreditManagerV2(manager).creditAccounts(address(this));
+        (value_,) = ICreditFacade(facade).calcTotalValue(account);
+    }
 }
