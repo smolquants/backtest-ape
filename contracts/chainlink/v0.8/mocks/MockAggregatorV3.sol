7bd1076030863eb383c9e235b5037782e1a47e06
fmrmf <m@fmrmf.xyz>
2023-02-10 10:57:56 -0500

    style: lint mock aggregator v3

--- contracts/chainlink/v0.8/mocks/MockAggregatorV3.sol
+++ contracts/chainlink/v0.8/mocks/MockAggregatorV3.sol
@@ -8,7 +8,7 @@ contract MockAggregatorV3 is Setter, AggregatorV3Interface {
     uint8 public immutable decimals;
     uint256 public immutable version;
     string public description;
-    
+
     struct RoundData {
         uint80 roundId;
         int256 answer;
@@ -18,20 +18,30 @@ contract MockAggregatorV3 is Setter, AggregatorV3Interface {
     }
     mapping(uint80 => RoundData) public rounds;
     uint80 public latestRoundId;
-    
+
     constructor(uint8 _decimals, string memory _description, uint256 _version) {
         decimals = _decimals;
         description = _description;
         version = _version;
     }
-        
-    function getRoundData(uint80 _roundId) public view returns (uint80 roundId_, int256 answer_, uint256 startedAt_, uint256 updatedAt_, uint80 answeredInRound_) {
+
+    function getRoundData(
+        uint80 _roundId
+    )
+        public
+        view
+        returns (uint80 roundId_, int256 answer_, uint256 startedAt_, uint256 updatedAt_, uint80 answeredInRound_)
+    {
         RoundData memory round = rounds[_roundId];
         (roundId_, answer_, startedAt_, updatedAt_, answeredInRound_) = (
-            round.roundId, round.answer, round.startedAt, round.updatedAt, round.answeredInRound
+            round.roundId,
+            round.answer,
+            round.startedAt,
+            round.updatedAt,
+            round.answeredInRound
         );
     }
-    
+
     function latestRoundData() public view returns (uint80, int256, uint256, uint256, uint80) {
         return getRoundData(latestRoundId);
     }
