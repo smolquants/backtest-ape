3f6d888e326926261e2e4fb9ed1e3d73de19eddc
fmrmf <m@fmrmf.xyz>
2023-08-24 06:49:00 -0400

    fix univ3 lp runner tests

--- backtest_ape/uniswap/v3/__init__.py
+++ backtest_ape/uniswap/v3/__init__.py
@@ -1,7 +1,8 @@
 from backtest_ape.uniswap.v3.base import BaseUniswapV3Runner
-from backtest_ape.uniswap.v3.lp import UniswapV3LPRunner
+from backtest_ape.uniswap.v3.lp import UniswapV3LPTotal0Runner, UniswapV3LPTotal1Runner
 
 __all__ = [
     "BaseUniswapV3Runner",
-    "UniswapV3LPRunner",
+    "UniswapV3LPTotal0Runner",
+    "UniswapV3LPTotal1Runner",
 ]
---
+++ backtest_ape/uniswap/v3/lp/__init__.py
@@ -0,0 +1,9 @@
+from backtest_ape.uniswap.v3.lp.total import (
+    UniswapV3LPTotal0Runner,
+    UniswapV3LPTotal1Runner,
+)
+
+__all__ = [
+    "UniswapV3LPTotal0Runner",
+    "UniswapV3LPTotal1Runner",
+]
