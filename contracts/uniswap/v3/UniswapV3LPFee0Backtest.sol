89ef7a70d7d976ccf202ca4b7dacfd5238f10293
fmrmf <m@fmrmf.xyz>
2023-08-25 12:56:14 -0400

    feat: univ3 lp backtest contracts for fee, principal

---
+++ contracts/uniswap/v3/UniswapV3LPFee0Backtest.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.12;
+
+import {UniswapV3LPBacktest} from "./UniswapV3LPBacktest.sol";
+import {MockPositionValue as PositionValue} from "./mocks/libraries/MockPositionValue.sol";
+
+/// @title Uniswap V3 Liquidity Provider Token0 Fee Backtester
+/// @notice Backtests an LP position in pool reporting accumulated fees token0 value of LP tokens
+contract UniswapV3LPFee0Backtest is UniswapV3LPBacktest {
+    constructor(address _manager) UniswapV3LPBacktest(_manager) {}
+
+    /// @notice Reports the accumulated fee token0 value of the LP tokens owned by this contract
+    /// @return value_ The current token0 accumulated fees of the LP tokens owned by this contract
+    function value() public view virtual override returns (uint256 value_) {
+        for (uint256 i = 0; i < tokenIds.length; ++i) {
+            uint256 tokenId = tokenIds[i];
+            (, , address token0, address token1, uint24 fee, , , , , , , ) = manager.positions(tokenId);
+
+            // get the sqrt price
+            address pool = getPool(token0, token1, fee);
+            uint160 sqrtRatioX96 = getSqrtRatioX96(pool);
+
+            (uint256 amount0Fee, uint256 amount1Fee) = PositionValue.fees(manager, tokenId);
+            value_ += amount0Fee;
+        }
+    }
+}
---
+++ contracts/uniswap/v3/UniswapV3LPPrincipal1Backtest.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.12;
+
+import {UniswapV3LPBacktest} from "./UniswapV3LPBacktest.sol";
+import {MockPositionValue as PositionValue} from "./mocks/libraries/MockPositionValue.sol";
+
+/// @title Uniswap V3 Liquidity Provider Token1 Principal Backtester
+/// @notice Backtests an LP position in pool reporting principal token1 value of LP tokens
+contract UniswapV3LPPrincipal1Backtest is UniswapV3LPBacktest {
+    constructor(address _manager) UniswapV3LPBacktest(_manager) {}
+
+    /// @notice Reports the principal token1 value of the LP tokens owned by this contract
+    /// @return value_ The current token1 principal of the LP tokens owned by this contract
+    function value() public view virtual override returns (uint256 value_) {
+        for (uint256 i = 0; i < tokenIds.length; ++i) {
+            uint256 tokenId = tokenIds[i];
+            (, , address token0, address token1, uint24 fee, , , , , , , ) = manager.positions(tokenId);
+
+            // get the sqrt price
+            address pool = getPool(token0, token1, fee);
+            uint160 sqrtRatioX96 = getSqrtRatioX96(pool);
+
+            (uint256 amount0Principal, uint256 amount1Principal) = PositionValue.principal(
+                manager,
+                tokenId,
+                sqrtRatioX96
+            );
+            value_ += amount1Principal;
+        }
+    }
+}
