05d0e22c3e18c93eef874abe90e3d863d1a401d4
fmrmf <m@fmrmf.xyz>
2023-08-27 10:47:06 -0400

    chore: bump to v0.1.0a12

--- backtest_ape/uniswap/v3/__init__.py
+++ backtest_ape/uniswap/v3/__init__.py
@@ -1,8 +1,5 @@
 from backtest_ape.uniswap.v3.base import BaseUniswapV3Runner
-from backtest_ape.uniswap.v3.lp import (
-    UniswapV3LPBaseRunner,
-    UniswapV3LPTotalRunner,
-)
+from backtest_ape.uniswap.v3.lp import UniswapV3LPBaseRunner, UniswapV3LPTotalRunner
 
 __all__ = [
     "BaseUniswapV3Runner",
